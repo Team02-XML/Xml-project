39ae85ef222d60abe76f5db8fe31ba291b0c5bb5
Sara Abdulmajeed <Sara.Alnasser@tuwaiq.edu.sa>
2021-07-01 16:22:42 +0300

    Create

--- XML_Project/Program.cs
+++ XML_Project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml;
 
 namespace XML_Project
 {
@@ -6,7 +8,63 @@ namespace XML_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateXMLFile();
+            ReadXMLFile();
+
         }
+        public static void CreateXMLFile()
+        {
+            List<string> studentNames = new List<string>{"Norah", "Abdulrahman", "Mutaz", "Adel", "Sara" };
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlElement root = xmlDoc.CreateElement("html");
+            xmlDoc.AppendChild(root);
+
+            XmlElement head = xmlDoc.CreateElement("head");
+            root.AppendChild(head);
+
+
+            XmlElement h1 = xmlDoc.CreateElement("h1");
+            XmlText hText = xmlDoc.CreateTextNode("TUWAIQ STUDENTS");
+            head.AppendChild(h1);
+            h1.AppendChild(hText);
+
+
+            XmlElement students = xmlDoc.CreateElement("students");
+            root.AppendChild(students);
+
+            foreach (var name in studentNames)
+            {
+                XmlElement student = xmlDoc.CreateElement("student");
+                XmlElement studentName = xmlDoc.CreateElement("name");
+                XmlText stNameText = xmlDoc.CreateTextNode(name);
+                student.AppendChild(studentName);
+                studentName.AppendChild(stNameText);
+                students.AppendChild(student);
+
+            }
+           
+
+          
+
+
+
+            xmlDoc.Save(@"");
+            Console.WriteLine(xmlDoc.InnerXml);
+        }
+
+        static void ReadXMLFile()
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(@"");
+            foreach (XmlNode node in xmldoc.DocumentElement.ChildNodes)
+            {
+                string element = node.Name;
+                Console.Write(element + ": ");
+                string text = node.InnerText;
+                Console.WriteLine(text);
+            }
+
+        }
+       
     }
 }
