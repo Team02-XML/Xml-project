fd65be8c71318fea80cc5d7b50b92e12ed308d2b
Norah <Norahmutairi@outlook.sa>
2021-07-01 15:57:51 +0300

    Create project

---
+++ XML_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
