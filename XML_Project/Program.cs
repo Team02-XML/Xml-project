7f7046cd23f4b5d8b0a5f8d95c5fed6d33a83768
Sara Abdulmajeed <Sara.Alnasser@tuwaiq.edu.sa>
2021-07-01 17:15:40 +0300

    Functions

--- XML_Project/Program.cs
+++ XML_Project/Program.cs
@@ -7,41 +7,37 @@ namespace XML_Project
 {
     class Program
     {
+        static XmlDocument xmlDoc = new XmlDocument();
+        static XmlElement root = xmlDoc.CreateElement("html");
+        static List<string> studentNames = new List<string> { "Norah", "Abdulrahman", "Mutaz", "Adel", "Sara" };
+
         public static void CreateXMLFile()
         {
-            List<string> studentNames = new List<string> { "Norah", "Abdulrahman", "Mutaz", "Adel", "Sara" };
-            XmlDocument xmlDoc = new XmlDocument();
-            XmlElement root = xmlDoc.CreateElement("html");
             xmlDoc.AppendChild(root);
-
             XmlElement head = xmlDoc.CreateElement("head");
             root.AppendChild(head);
-
-
             XmlElement h1 = xmlDoc.CreateElement("h1");
             XmlText hText = xmlDoc.CreateTextNode("TUWAIQ STUDENTS");
             head.AppendChild(h1);
             h1.AppendChild(hText);
-
-
-            XmlElement students = xmlDoc.CreateElement("students");
-            root.AppendChild(students);
-
+            CreateStudent();
+            xmlDoc.Save(@"D:\\XMLProject.xml");
+            Console.WriteLine(xmlDoc.InnerXml);
+        }
+        static void CreateStudent()
+        {
             foreach (var name in studentNames)
             {
+                XmlElement students = xmlDoc.CreateElement("students");
+                root.AppendChild(students);
                 XmlElement student = xmlDoc.CreateElement("student");
                 XmlElement studentName = xmlDoc.CreateElement("name");
                 XmlText stNameText = xmlDoc.CreateTextNode(name);
                 student.AppendChild(studentName);
                 studentName.AppendChild(stNameText);
                 students.AppendChild(student);
-
             }
-
-            xmlDoc.Save(@"D:\\XMLProject.xml");
-            Console.WriteLine(xmlDoc.InnerXml);
         }
-
         static void ReadXMLFile()
         {
             XmlDocument xmldoc = new XmlDocument();
@@ -64,9 +60,7 @@ namespace XML_Project
             writerThread.Join();
             Thread readThread = new Thread(ReadXMLFile);
             readThread.Start();
-
             ReadXMLFile();
-
         }
        
     }
